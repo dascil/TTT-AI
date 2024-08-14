d4e6f5e908f18dc99538106c257551ab1df83f1d
dascil <zacharydascil@protonmail.com>
2024-08-14 07:45:22 -0700

    Added methods to board.py
    -Added wins variable
    -Added swapPlayers
    -Added startingPlayer
    -Added printBoardIndex

--- board.py
+++ board.py
@@ -1,7 +1,23 @@
+import random as r
+
 class Board:
     def __init__(self):
         self.boardValues = ["_"] * 9
         self.currentPlayer = ""
+        self.wins = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+
+    def startingPlayer(self):
+        """
+        Decides starting player for game
+        """
+        startingPlayer = r.randint(0,1)
+        self.currentPlayer = "X" if startingPlayer == 0 else "O"
+
+    def swapPlayers(self):
+        """
+        Changes to other player
+        """
+        self.currentPlayer = "X" if self.currentPlayer == "O" else "O"
 
     def printCurrentBoard(self):
         """
@@ -19,5 +35,24 @@ class Board:
         print("-|-----|-----|-----|-")
         print(" |     |     |     | ")
         print(" |  {}  |  {}  |  {}  | ".format(self.boardValues[6], self.boardValues[7], self.boardValues[8]))
+        print(" |     |     |     | ")
+        print("-|-----|-----|-----|-")
+
+    def printBoardIndexes(self):
+        """
+        Prints out the current game board.
+        Takes in the values from boardValues.
+        """
+        print("-|-----|-----|-----|-")
+        print(" |     |     |     | ")
+        print(" |  1  |  2  |  3  | ")
+        print(" |     |     |     | ")
+        print("-|-----|-----|-----|-")
+        print(" |     |     |     | ")
+        print(" |  4  |  5  |  6  | ")
+        print(" |     |     |     | ")
+        print("-|-----|-----|-----|-")
+        print(" |     |     |     | ")
+        print(" |  7  |  8  |  9  | ")
         print(" |     |     |     | ")
         print("-|-----|-----|-----|-")
