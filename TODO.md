e89fcc5d73336cacc124a8ae841dec07bbf6e97d
dascil <zacharydascil@protonmail.com>
2024-08-16 08:07:10 -0700

    Added comments to TODO and Main file
    - Added main file comments and method
    - Added todo

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- Minimax algorithm with depth selection and alpha beta pruning
+- GUI
--- TTT-AI.py
+++ TTT-AI.py
@@ -1,1 +1,10 @@
-# This file will serve as the main script file.
+def TTT_AI():
+    print("Welcome to Tic-Tac-Toe")
+    # Prompt user to pick X or O
+    # Decide who goes first
+    # Initiate Game
+    # Keep going until an exit state is found
+    # Prompt user to choose if they want to play again
+
+if __name__ == "__main__":
+    TTT_AI()
